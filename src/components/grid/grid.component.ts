604979957b30bdbfc9cad34a1bfeef9e49560a8a
Tony Barrett <tonyb@gmx.com>
2025-02-03 15:47:15 +0000

    inputs working

--- src/components/grid/grid.component.ts
+++ src/components/grid/grid.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../card/card.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [CardComponent],
+  imports: [
+    FormsModule, 
+    CardComponent
+  ],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
 export class GridComponent {
   cards: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  numCards: number = 0;
+  numColumns: number = 0;
+
+  ngOnInit():void {}
+
+  changeGrid():void {
+    this.numCards = Math.trunc(this.numCards);
+    this.numColumns = Math.trunc(this.numColumns);
+    console.log('cards: ', this.numCards);
+    console.log('columns: ', this.numColumns);
+  }
+
+
 
 }
