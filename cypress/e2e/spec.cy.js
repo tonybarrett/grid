2597ca5908793b99eb563be71e2a6b26c2a2ccdb
Tony Barrett <tonyb@gmx.com>
2025-10-01 13:25:53 +0100

    Test syntax made clearer

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,11 @@
 it ('my first test', () => {
     cy.visit('/')
-    cy.get('[name="num_cards"]').click();
-    cy.get('[name="num_cards"]').clear();
-    cy.get('[name="num_cards"]').type('33');
-    cy.get('[name="num_columns"]').click();
-    cy.get('[name="num_columns"]').clear();
-    cy.get('[name="num_columns"]').type('5');
+    cy.get('#num_cards').click();
+    cy.get('#num_cards').clear();
+    cy.get('#num_cards').type('33');
+    cy.get('#num_columns').click();
+    cy.get('#num_columns').clear();
+    cy.get('#num_columns').type('5');
 
     cy.get('app-card:nth-child(33) #card')
         .should('be.visible');
