a98f6e6f4e7909321382f87fdf71c3bc93cc62cc
Tony Barrett <tonyb@gmx.com>
2025-10-01 11:45:18 +0100

    Test added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,13 @@
 it ('my first test', () => {
-    cy.visit('http://localhost:4200')
+    cy.visit('/')
+    cy.get('[name="num_cards"]').click();
+    cy.get('[name="num_cards"]').clear();
+    cy.get('[name="num_cards"]').type('33');
+    cy.get('[name="num_columns"]').click();
+    cy.get('[name="num_columns"]').clear();
+    cy.get('[name="num_columns"]').type('5');
+
+    cy.get('app-card:nth-child(33) #card')
+        .should('be.visible');
 })
 
