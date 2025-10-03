49439238b615c0ea290cf969517659e4c6bd9b62
Tony Barrett <tonyb@gmx.com>
2025-10-03 09:28:48 +0100

    Cypress best practice

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,11 +1,11 @@
 it ('are the cards created and displayed', () => {
     cy.visit('/')
-    cy.get('#num_cards').click();
-    cy.get('#num_cards').clear();
-    cy.get('#num_cards').type('33');
-    cy.get('#num_columns').click();
-    cy.get('#num_columns').clear();
-    cy.get('#num_columns').type('5');
+    cy.get('[data-cy="num_cards"]').click();
+    cy.get('[data-cy="num_cards"]').clear();
+    cy.get('[data-cy="num_cards"]').type('33');
+    cy.get('[data-cy="num_columns"]').click();
+    cy.get('[data-cy="num_columns"]').clear();
+    cy.get('[data-cy="num_columns"]').type('5');
 
     cy.get('app-card:last #card')
         .should('be.visible');
